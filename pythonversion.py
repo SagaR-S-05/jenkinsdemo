4d08aaaf27946dbd3cc8ba67e3c860189c304f2b
Sagar S <sagarsuma11@gmail.com>
2024-12-10 11:48:14 +0530

    version

---
+++ pythonversion.py
@@ -0,0 +1,9 @@
+import sys
+
+def display_python_version():
+    version = sys.version
+    print("Current Python version:")
+    print(version)
+
+if __name__ == "__main__":
+    display_python_version()
